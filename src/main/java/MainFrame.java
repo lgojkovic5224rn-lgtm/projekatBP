1f5d2239d6ebaf166ce46cf8d82930c5e2c3c678
lgojkovic5224rn-lgtm <lgojkovic5224rn@raf.rs>
2026-05-04 19:33:34 +0200

    test 2

--- src/main/java/MainFrame.java
+++ src/main/java/MainFrame.java
@@ -16,7 +16,7 @@ public class MainFrame extends JFrame {
         this.setContentPane(panel);
 
         // Osnovna podešavanja prozora
-        this.setTitle("Projekat Baze");
+        this.setTitle("Projekat Test 1");
         this.setSize(800, 600);
         this.setLocationRelativeTo(null); // centriranje
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
